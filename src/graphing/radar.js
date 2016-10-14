41d493b9024eff6c5ebdb88909c3ea2cf141a0ef
lionescu <lrionescu26@gmail.com>
2016-10-14 13:55:33 +0100

    Medina/Laura: Add legend

--- src/graphing/radar.js
+++ src/graphing/radar.js
@@ -7,7 +7,6 @@ const RingCalculator = require('../util/ringCalculator');
 
 const Radar = function (size, radar) {
   var svg, radarElement, blipWidth = 22;
-  var leftAlign = '18.5%';
 
   var tip = d3tip().attr('class', 'd3-tip').html(function (text) {
     return text;
@@ -109,10 +108,23 @@ const Radar = function (size, radar) {
       .attr('class', order);
   }
 
+  function triangleLegend(x, y, order, group) {
+    return group.append('path').attr('d', "M412.201,311.406c0.021,0,0.042,0,0.063,0c0.067,0,0.135,0,0.201,0c4.052,0,6.106-0.051,8.168-0.102c2.053-0.051,4.115-0.102,8.176-0.102h0.103c6.976-0.183,10.227-5.306,6.306-11.53c-3.988-6.121-4.97-5.407-8.598-11.224c-1.631-3.008-3.872-4.577-6.179-4.577c-2.276,0-4.613,1.528-6.48,4.699c-3.578,6.077-3.26,6.014-7.306,11.723C402.598,306.067,405.426,311.406,412.201,311.406")
+      .attr('transform', 'scale(' + (blipWidth / 64) + ') translate(' + (-404 + x * (64 / blipWidth) - 17) + ', ' + (-282 + y * (64 / blipWidth) - 17) + ')')
+      .attr('class', order);
+  }
+
   function circle(x, y, order, group) {
     return (group || svg).append('path')
       .attr('d', "M420.084,282.092c-1.073,0-2.16,0.103-3.243,0.313c-6.912,1.345-13.188,8.587-11.423,16.874c1.732,8.141,8.632,13.711,17.806,13.711c0.025,0,0.052,0,0.074-0.003c0.551-0.025,1.395-0.011,2.225-0.109c4.404-0.534,8.148-2.218,10.069-6.487c1.747-3.886,2.114-7.993,0.913-12.118C434.379,286.944,427.494,282.092,420.084,282.092")
       .attr('transform', 'scale(' + (blipWidth / 34) + ') translate(' + (-404 + x * (34 / blipWidth) - 17) + ', ' + (-282 + y * (34 / blipWidth) - 17) + ')')
+      .attr('class', order);
+  }
+
+  function circleLegend(x, y, order, group) {
+    return (group || svg).append('path')
+      .attr('d', "M420.084,282.092c-1.073,0-2.16,0.103-3.243,0.313c-6.912,1.345-13.188,8.587-11.423,16.874c1.732,8.141,8.632,13.711,17.806,13.711c0.025,0,0.052,0,0.074-0.003c0.551-0.025,1.395-0.011,2.225-0.109c4.404-0.534,8.148-2.218,10.069-6.487c1.747-3.886,2.114-7.993,0.913-12.118C434.379,286.944,427.494,282.092,420.084,282.092")
+      .attr('transform', 'scale(' + (blipWidth / 64) + ') translate(' + (-404 + x * (64 / blipWidth) - 17) + ', ' + (-282 + y * (64 / blipWidth) - 17) + ')')
       .attr('class', order);
   }
 
@@ -268,76 +280,53 @@ const Radar = function (size, radar) {
     d3.select('.legend').remove();
   }
 
-  function plotRadarLegend(pageElement, order) {
+  function drawLegend(header, order) {
     removeRadarLegend();
-
-    pageElement
-      .insert('div', '.home-link + *')
-      .classed('legend', true)
-      .classed(order, true)
-      .html('<img src="/images/radar_legend.png" />');
-  }
-
-  var drawKey = function (header, moveConst, colour, order) {
-
-    d3.selectAll('.legend').remove();
 
     var triangleKey = "New or moved";
     var circleKey = "No change";
 
-    console.log(moveConst);
+    var container = header.append('svg')
+      .attr('class', 'legend legend'+"-"+order)
+      .attr('width', '200px')
+      .attr('height', '100%');
 
-    var container = d3.select('svg').append('g').attr('class', 'legend');
+    var x = 10;
+    var y = 10;
+    var colour = '#000';
 
-    var x = 404;
-    var y = 0;
-
-    if (order == 'first') {
-      x += 1 * moveConst / 10;
-      y += 1 * moveConst / 10;
-    }
-    else if (order == "second") {
-      x += 1 * moveConst / 10;
-      y += 1 * moveConst / 10;
-    }
-    else if (order == "third") {
-      x += 1 * moveConst / 5;
-      y += 3 * moveConst / 5;
-    }
-    else if (order == "fourth") {
-      x += 2 * moveConst / 5;
-      y += 4 * moveConst / 5;
+    if(order == "second" || order == "third") {
+      x = 0.7 * window.innerWidth;
     }
 
-
-    triangle(x, y, 'black', container)
+    triangleLegend(x, y, 'black', container)
       .attr('fill', colour);
 
     container
       .append('text')
-      .attr('x', x + 10)
-      .attr('y', y + 5)
+     .attr('x', x + 15)
+     .attr('y', y + 5)
       .attr('fill', colour)
       .attr('font-size', '0.8em')
       .text(triangleKey);
 
 
-    circle(x, y + 30, 'black', container)
+    circleLegend(x, y + 20, 'black', container)
       .attr('fill', colour);
 
     container
       .append('text')
-      .attr('x', x + 10)
-      .attr('y', y + 35)
+      .attr('x', x + 15)
+      .attr('y', y + 25)
       .attr('fill', colour)
       .attr('font-size', '0.8em')
       .text(circleKey);
-  };
+  }
 
   function redrawFullRadar() {
     removeHomeLink();
-    // removeRadarLegend();
-    // plotRadarLegend(d3.select('header'), 'first');
+    removeRadarLegend();
+    drawLegend(d3.select('header'), 'first');
 
     svg.style('left', 0).style('right', 0);
 
@@ -471,13 +460,7 @@ const Radar = function (size, radar) {
       .style('pointer-events', 'none')
       .attr('transform', 'translate(' + translateXAll + ',' + translateYAll + ')scale(0)');
 
-
-    // console.log("move left =" + moveLeft);
-    // console.log("move right =" + moveRight);
-    // console.log("translateX =" + translateX);
-    // console.log("translateY =" + translateY);
-    // drawKey(d3.select('header'), Math.abs(translateX)/2, translateX, translateY, '#000', order);
-    drawKey(d3.select('header'), Math.max(moveLeft, moveRight), '#000', order);
+    drawLegend(d3.select('header'), Math.max(moveLeft, moveRight), order);
   }
 
   self.init = function () {
@@ -493,7 +476,7 @@ const Radar = function (size, radar) {
     var header = plotRadarHeader();
 
     plotQuadrantButtons(quadrants, header);
-    // plotRadarLegend(header, 'first');
+    drawLegend(d3.select('header'), 'first');
 
     radarElement.style('height', size + 14 + 'px');
     svg = radarElement.append("svg").call(tip);
